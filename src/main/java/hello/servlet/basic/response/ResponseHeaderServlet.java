358706a91f0149f1dd1af34be14b4c5a8f6417e1
lazzzykim <33sunghyeon@gmail.com>
2025-04-16 15:20:16 +0900

    HttpServletResponse

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -35,27 +34,27 @@ public class ResponseHeaderServlet extends HttpServlet {
     }
 
     private void content(HttpServletResponse response) {
-//Content-Type: text/plain;charset=utf-8
-//Content-Length: 2
-//response.setHeader("Content-Type", "text/plain;charset=utf-8");
+    //Content-Type: text/plain;charset=utf-8
+    //Content-Length: 2
+    //response.setHeader("Content-Type", "text/plain;charset=utf-8");
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
-//response.setContentLength(2); //(생략시 자동 생성)
+    //response.setContentLength(2); //(생략시 자동 생성)
     }
 
     private void cookie(HttpServletResponse response) {
-//Set-Cookie: myCookie=good; Max-Age=600;
-//response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+    //Set-Cookie: myCookie=good; Max-Age=600;
+    //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
         Cookie cookie = new Cookie("myCookie", "good");
         cookie.setMaxAge(600); //600초
         response.addCookie(cookie);
     }
 
     private void redirect(HttpServletResponse response) throws IOException {
-//Status Code 302
-//Location: /basic/hello-form.html
-//        response.setStatus(HttpServletResponse.SC_FOUND); //302
-//        response.setHeader("Location", "/basic/hello-form.html");
+    //Status Code 302
+    //Location: /basic/hello-form.html
+    //response.setStatus(HttpServletResponse.SC_FOUND); //302
+    //response.setHeader("Location", "/basic/hello-form.html");
         response.sendRedirect("/basic/hello-form.html");
     }
 }
